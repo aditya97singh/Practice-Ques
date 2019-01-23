b8144a9e712b3ee620e1903b9b8d59f881414513
aditya97singh <46920079+aditya97singh@users.noreply.github.com>
2019-01-23 15:17:08 +0530

    Add files via upload

---
+++ Oc.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Oc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+    String s=sc.next();
+    int l=s.length();
+    int c=0,max=0;
+    for(int i=0;i<l;i++)
+    {
+    	c=0;
+    	for(int j=0;j<l;j++)
+    	{
+    		if(s.charAt(i)==s.charAt(j))
+    		 c++;
+    			
+    	}
+    	if(c>max)
+    	{
+    	 max=c;
+    	 System.out.println(s.charAt(i)+" "+max);
+    	}
+    	}
+	}
+
+}
