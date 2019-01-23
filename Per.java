6a5b3d4cc48bfe8c850594c36a501b8290ca0714
aditya97singh <46920079+aditya97singh@users.noreply.github.com>
2019-01-23 14:51:32 +0530

    Add files via upload

---
+++ Per.java
@@ -0,0 +1,20 @@
+
+public class Per {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int sum=0;
+    for(int i=1;i<100;i++)
+    {
+    	sum=0;
+    	for(int j=1;j<i;j++)
+    	{
+    		if(i%j==0)
+    			sum=sum+j;
+    	}
+    	if(sum==i)
+    		System.out.println(sum);
+    }
+	}
+
+}
